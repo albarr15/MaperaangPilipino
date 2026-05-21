666d3332bd5429a008c8ead03f644fefecdaa1bc
JorellAndreiFinez <jorellandrei23@gmail.com>
2026-05-21 20:38:55 +0800

    chore(vercel): deploy website in vercel

--- src/components/navbar/MobileMenu.tsx
+++ src/components/navbar/MobileMenu.tsx
@@ -7,12 +7,24 @@ interface MobileMenuProps {
 const MobileMenu: React.FC<MobileMenuProps> = ({ closeMenu }) => {
   return (
     <div className="md:hidden flex flex-col bg-black px-6 py-4 space-y-3">
-      <a href="/" onClick={closeMenu}>Home</a>
-      <a href="/about" onClick={closeMenu}>About</a>
-      <a href="/accomplishments" onClick={closeMenu}>Accomplishments</a>
-      <a href="/careers" onClick={closeMenu}>Careers</a>
-      <a href="/services" onClick={closeMenu}>Services</a>
-      <a href="/contact" onClick={closeMenu}>Contact</a>
+      <a href="/" onClick={closeMenu}>
+        Home
+      </a>
+      <a href="/about" onClick={closeMenu}>
+        About
+      </a>
+      <a href="/accomplishments" onClick={closeMenu}>
+        Accomplishments
+      </a>
+      <a href="/careers" onClick={closeMenu}>
+        Careers
+      </a>
+      <a href="/services" onClick={closeMenu}>
+        Services
+      </a>
+      <a href="/contact" onClick={closeMenu}>
+        Contact
+      </a>
     </div>
   );
 };
